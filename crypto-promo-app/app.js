eb42be405cb2b6be5c6aa5ddcbe4dbc79fde9daa
bcdevzidan <154742697+bcdevzidan@users.noreply.github.com>
2023-12-31 18:58:21 +0700

    Add files via upload

---
+++ crypto-promo-app/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Middleware untuk menyajikan file statis
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mengirim file index.html saat mengakses root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
